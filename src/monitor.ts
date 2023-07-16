2973044d7645c006cf5c26a2df7226f2fc2272e9
Stefan Freitag <stefan.freitag@udo.edu>
2023-07-16 20:41:30 +0200

    fix: updated integration test
    - Updated the integration test command in README.md and API.md to include the correct directory path.
    - Renamed the file `test/integ.monitor.test.ts` to `integ-tests/integ.monitor.test.ts`.
    - Fixed a missing newline at the end of `src/index.ts`.
    - Made minor formatting changes in `src/monitor.ts` for consistency.
    - Updated the display name of the SNS topic to 'fsx-lifecycle-monitor' in `test/monitor.test.ts`.

--- src/monitor.ts
+++ src/monitor.ts
@@ -1,14 +1,14 @@
-import * as cdk from "aws-cdk-lib";
-import * as kms from "aws-cdk-lib/aws-kms";
-import * as events from "aws-cdk-lib/aws-events";
-import * as targets from "aws-cdk-lib/aws-events-targets";
+import * as path from 'path';
+import * as cdk from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as targets from 'aws-cdk-lib/aws-events-targets';
 
-import * as iam from "aws-cdk-lib/aws-iam";
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as sns from "aws-cdk-lib/aws-sns";
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sns from 'aws-cdk-lib/aws-sns';
 
-import { Construct } from "constructs";
-import path = require("path");
+import { Construct } from 'constructs';
 
 export class FsxLifecycleStatusMonitor extends Construct {
   fn: lambda.Function;
@@ -27,14 +27,14 @@ export class FsxLifecycleStatusMonitor extends Construct {
     this.fn = this.createLambdaFunction();
     this.fn.role?.attachInlinePolicy(this.policy);
 
-    new events.Rule(this, "rule", {
-      ruleName: "fsx-health-trigger",
+    new events.Rule(this, 'rule', {
+      ruleName: 'fsx-health-trigger',
       schedule: events.Schedule.cron({
-        minute: "0/10",
-        hour: "*",
-        day: "*",
-        month: "*",
-        year: "*",
+        minute: '0/10',
+        hour: '*',
+        day: '*',
+        month: '*',
+        year: '*',
       }),
       targets: [new targets.SnsTopic(this.topic)],
     });
@@ -47,59 +47,59 @@ export class FsxLifecycleStatusMonitor extends Construct {
    * @memberof FsxLifecycleStatusMonitor - class instance
    */
   createSNSTopic(): sns.Topic {
-    return new sns.Topic(this, "fsx-health-sns-topic", {
-      displayName: "fsx-lifecycle-monitor",
-      topicName: "fsx-lifecycle-monitor",
-      masterKey: kms.Alias.fromAliasName(this, "alias", "alias/aws/sns"),
+    return new sns.Topic(this, 'fsx-health-sns-topic', {
+      displayName: 'fsx-lifecycle-monitor',
+      topicName: 'fsx-lifecycle-monitor',
+      masterKey: kms.Alias.fromAliasName(this, 'alias', 'alias/aws/sns'),
     });
   }
 
   createIamPolicy(): iam.Policy {
     const stack = cdk.Stack.of(this);
-    return new iam.Policy(this, "policy", {
-      policyName: "fsx-lambda-sns",
+    return new iam.Policy(this, 'policy', {
+      policyName: 'fsx-lambda-sns',
       statements: [
         new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
           actions: [
-            "logs:CreateLogGroup",
-            "logs:CreateLogStream",
-            "logs:PutLogEvents",
+            'logs:CreateLogGroup',
+            'logs:CreateLogStream',
+            'logs:PutLogEvents',
           ],
           resources: [
-            "arn:aws:logs:" +
+            'arn:aws:logs:' +
               stack.region +
-              ":" +
+              ':' +
               stack.account +
-              ":log-group:/aws/lambda/*",
+              ':log-group:/aws/lambda/*',
           ],
         }),
         new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
-          actions: ["fsx:DescribeFileSystems"],
-          resources: ["*"],
+          actions: ['fsx:DescribeFileSystems'],
+          resources: ['*'],
         }),
         new iam.PolicyStatement({
           effect: iam.Effect.ALLOW,
-          actions: ["sns:Publish"],
-          resources: ["*"],
+          actions: ['sns:Publish'],
+          resources: ['*'],
         }),
       ],
     });
   }
 
   /**
-   * 
+   *
    *
    * @return {*}  {lambda.Function}
    * @memberof FsxLifecycleStatusMonitor
    */
   createLambdaFunction(): lambda.Function {
-    return new lambda.Function(this, "fsx-lifecycle-monitor", {
+    return new lambda.Function(this, 'fsx-lifecycle-monitor', {
       runtime: lambda.Runtime.PYTHON_3_10,
-      handler: "fsx-health.lambda_handler",
+      handler: 'fsx-health.lambda_handler',
       architecture: lambda.Architecture.X86_64,
-      code: lambda.Code.fromAsset(path.join(__dirname, "../lambda")),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../lambda')),
       insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_229_0,
       environment: {
         LambdaSNSTopic: this.topic.topicArn,
